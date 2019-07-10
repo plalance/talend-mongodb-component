a62bd3b5c576932871bc90090a59d943c02bae02
plalance <plalance@cerp-rrm.com>
2019-07-10 18:08:46 +0200

    now accepts query and empty query to retrieve datas : json format

--- src/main/java/com/cerp/components/source/MongoDbInputSource.java
+++ src/main/java/com/cerp/components/source/MongoDbInputSource.java
@@ -39,17 +39,16 @@ public class MongoDbInputSource implements Serializable {
 
     @PostConstruct
     public void init() {
-        // this method will be executed once for the whole component execution,
-        // this is where you can establish a connection for instance
 
-
-        System.out.println("> MongoInput Component");
-        System.out.println("> Configuration :");
-        System.out.println(">> Host : " + configuration.getDatabase().getDatastore().getHost());
-        System.out.println(">> Username : " + configuration.getDatabase().getDatastore().getUsername());
-        System.out.println(">> Password : " + configuration.getDatabase().getDatastore().getPassword());
-        System.out.println(">> Database Used : " + configuration.getDatabase().getDatastore().getDatabase());
-        System.out.println(">> Collection Used : " + configuration.getDatabase().getDatastore().getCollection());
+        System.out.println("------< MongoInput Component >------");
+        System.out.println("__ Configuration :");
+        System.out.println("__ Host : " + configuration.getDatabase().getDatastore().getHost());
+        System.out.println("__ Username : " + configuration.getDatabase().getDatastore().getUsername());
+        System.out.println("__ Password : " + configuration.getDatabase().getDatastore().getPassword());
+        System.out.println("__ Database Used : " + configuration.getDatabase().getDatastore().getDatabase());
+        System.out.println("__ Collection Used : " + configuration.getDatabase().getDatastore().getCollection());
+        System.out.println("__ Query : " + configuration.getDatabase().getQuery());
+        System.out.println("------< >------");
 
         // MongoDb Client
         MongoClient client = MongoClients.create(configuration.getDatabase().getDatastore().getHost());
@@ -58,10 +57,24 @@ public class MongoDbInputSource implements Serializable {
         final MongoDatabase db = client.getDatabase(configuration.getDatabase().getDatastore().getDatabase());
         final MongoCollection<Document> coll = db.getCollection(configuration.getDatabase().getDatastore().getCollection());
 
+        // Bson Document List, target of coll.find() results.
         List<Document> docs = new ArrayList<>();
 
-        // Retrieve Data from Collection
-        coll.find().into(docs);
+
+        System.out.println("query is null ? " + (configuration.getDatabase().getQuery() == null));
+
+        String query = Optional
+                .ofNullable(configuration.getDatabase().getQuery()) // Value tested
+                .filter(s -> !s.isEmpty()) // Filter : string must not be emtpy
+                .orElse("{}"); // other cases : return empty json object.
+
+        System.out.println("Query prepared :" + query);
+
+        // Generate Bson Doc of query
+        Document queryDoc = Document.parse(query);
+
+        // get data
+        coll.find(queryDoc).into(docs);
 
         docs = Optional.ofNullable(docs).orElse(Collections.emptyList());
 
@@ -99,38 +112,8 @@ public class MongoDbInputSource implements Serializable {
 
     @Producer
     public JsonObject next() {
-/*
-        System.out.println("--- Producer ---");
-
-        JsonArrayBuilder builder = Json.createBuilderFactory(null).createArrayBuilder();
-
-        // MongoDb Client
-        MongoClient client = MongoClients.create("mongodb://127.0.0.1:27018");
-
-        // Connect to NFE204 base & get movies collection
-        MongoDatabase db = client.getDatabase("nfe204");
-        MongoCollection<Document> coll = db.getCollection("movies");
-
-        ArrayList<Document> docs = new ArrayList<Document>();
-
-        coll.find().into(docs);
-
-        for (Document doc : docs) {
-            System.out.println("Titre : " + doc.getString("title"));
-            if(doc.getString("title") != null)
-                builder.add(doc.getString("title"));
-        }
-
-        JsonArray response = builder.build();
-
-        JsonObject jo = Json.createObjectBuilder().add("titre", "yolooo super titre").build();
-        System.out.println("Objet retourné : " + jo);
-
-        return jo;*/
 
         JsonObject o = buffer.next();
-
-//        System.out.println("Next Buffer : " + o);
 
         return o;
     }
