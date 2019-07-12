08e49ce6d29a27913ca0a4e936e6179503c1504f
plalance <plalance@cerp-rrm.com>
2019-07-12 14:19:05 +0200

    JsonObject structure key/value dynamically generated based on MongoDb Document Collection returned

--- src/main/java/com/plalance/components/service/MongoComponentService.java
+++ src/main/java/com/plalance/components/service/MongoComponentService.java
@@ -1,14 +1,13 @@
 package com.plalance.components.service;
 
-import java.util.HashMap;
+import java.io.StringReader;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
-import javax.json.JsonValue;
+import javax.json.JsonReader;
 
 import org.bson.Document;
 import org.bson.json.JsonMode;
@@ -21,30 +20,37 @@ public class MongoComponentService {
 	public Stream<JsonObject> getDataAsStream(List<Document> list) {
 
 		System.out.println("------< MongoComponentservice : getDataStream() >------");
-
+		
+		/* JsonWriter Settings */
+		JsonWriterSettings relaxed = JsonWriterSettings.builder().outputMode(JsonMode.RELAXED).build();
+		
 		return list.stream() //
 				.map(doc -> {
+					
+					String jsonString = doc.toJson(relaxed);
+					
+					return jsonFromString(jsonString);
 
-					JsonObjectBuilder builder = Json.createObjectBuilder();
-
-					HashMap<String, Object> keys = new HashMap<>();
-					keys.put("id", doc.get("_id"));
-					keys.put("title", doc.get("title"));
-					keys.put("year", doc.get("year"));
-					keys.put("genre", doc.get("genre"));
-					keys.put("summary", doc.get("summary"));
-
-					for (Map.Entry<String, Object> e : keys.entrySet()) {
-						String key = e.getKey();
-						Object value = e.getValue();
-
-						if (value == null)
-							builder.add(key, JsonValue.NULL);
-						else
-							builder.add(key, value.toString());
-					}
-
-					return builder.build();
+//					JsonObjectBuilder builder = Json.createObjectBuilder();
+//
+//					HashMap<String, Object> keys = new HashMap<>();
+//					keys.put("id", doc.get("_id"));
+//					keys.put("title", doc.get("title"));
+//					keys.put("year", doc.get("year"));
+//					keys.put("genre", doc.get("genre"));
+//					keys.put("summary", doc.get("summary"));
+//
+//					for (Map.Entry<String, Object> e : keys.entrySet()) {
+//						String key = e.getKey();
+//						Object value = e.getValue();
+//
+//						if (value == null)
+//							builder.add(key, JsonValue.NULL);
+//						else
+//							builder.add(key, value.toString());
+//					}
+//
+//					return builder.build();
 				});
 	}
 
@@ -63,5 +69,14 @@ public class MongoComponentService {
 					return builder.build();
 
 				});
+	}
+	
+	private static JsonObject jsonFromString(String jsonObjectStr) {
+
+	    JsonReader jsonReader = Json.createReader(new StringReader(jsonObjectStr));
+	    JsonObject object = jsonReader.readObject();
+	    jsonReader.close();
+
+	    return object;
 	}
 }
